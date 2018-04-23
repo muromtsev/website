adcc59d8abfabd575225d8f377a28d34e658c603
Ilya <t4k.org@mail.ru>
2018-04-23 22:16:13 +0300

    test 23.04
    test 23.04

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//modal btn
 $('.js-click-modal').click(function(){
     console.log($('.js-click-modal'));
   $('.stock-list').addClass('modal-open');
@@ -7,33 +8,12 @@ $('.js-close-modal').click(function(){
   $('.stock-list').removeClass('modal-open');
 });
 
-// после загрузки страницы
-$(document).ready(function(){
-    // запускаем отображение аккордеона
-    makeAccordion();    
-    // при клике по заголовку
-    $('.accordion-title').click(function(){
-        // снимаем со всех div`ов класс active
-        $('.accordion').each(function(){
-            $(this).removeClass('active');
-        });
-        // задаем родительскому div`у класс active
-        $(this).parent().addClass('active');
-        // запускаем отображение аккордеона
-        makeAccordion();
-    });
+//accordion
+$(document).ready(function() {
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
 });
-// функция для отображения аккордеона. Сворачивает и разворачивает
-function makeAccordion(){
-    var speed = 300; // скорость анимации
-    // перебираем все блоки аккордеона
-    $('.accordion').each(function(){
-        if($(this).hasClass('active')){
-            // если находим активный, то разворачиваем его
-            $(this).find('.content').slideDown(speed);
-        }else{
-            // не активный сворачиваем
-            $(this).find('.content').slideUp(speed);
-        }
-    });
-}
+

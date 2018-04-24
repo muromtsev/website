865bf741848ab05470efe17d5760983c412bd984
Ilya <t4k.org@mail.ru>
2018-04-24 23:44:55 +0300

    24.04
    24.04

--- js/jssor-stock-slider.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-        var options = { $AutoPlay: 1,
-                        $ThumbnailNavigatorOptions: {
-                        $Class: $JssorThumbnailNavigator$,
-                        $ChanceToShow: 2
-                        }
-                      };
-        var jssor1_slider = new $JssorSlider$("jssor_1", options);
-    });
